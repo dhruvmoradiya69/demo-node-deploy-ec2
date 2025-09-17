d5071c20763cb999b7965d68aec8600fee634116
Dhruv Moradiya <dhruvmoradiya69000@gmail.com>
2025-09-17 22:23:43 +0530

    dedae

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,16 @@
 module.exports = {
   apps: [
     {
-      name: "backend",
+      name: "backend-dev",
       script: "./server.js",
       env: {
         NODE_ENV: "development",
         PORT: 3001,
       },
+    },
+    {
+      name: "backend-prod",
+      script: "./server.js",
       env_production: {
         NODE_ENV: "production",
         PORT: 3000,
